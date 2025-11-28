31f0f20c4226f4ac0f17f179de9539fca53fd9f1
KayG2310 <kamakshig2310@gmail.com>
2025-11-28 05:58:39 +0530

    hopefully final

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 app.use(cors({
-  origin: `${import.meta.env.VITE_REACT_APP_FRONTEND_BASE_URL}`,
+  origin: process.env.FRONTEND_URL,
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
