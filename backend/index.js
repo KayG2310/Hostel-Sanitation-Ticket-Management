431073c66f6f047440cc8e33a83902ec0c47e4ca
KayG2310 <kamakshig2310@gmail.com>
2025-11-28 06:09:10 +0530

    hopefully final

--- backend/index.js
+++ backend/index.js
@@ -32,5 +32,6 @@ app.get("/", (req, res) => {
 });
 app.use("/api/student", studentRoutes);
 app.use("/api/caretaker", caretakerRoutes);
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+export default app;
